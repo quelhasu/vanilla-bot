6b84fab34021bfd8b2b69e340bbc3dbea9776b7d
quelhasu <uquelhas@gmail.com>
2019-01-17 19:43:53 +0100

    🔥 Weather API

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
+const request = require('request');
+const api_secret = "3256f19960bc8409e6825ee8382d1389";
 
 const Readline = require('readline'); //for reading inputs 
 const rl = Readline.createInterface({ //for reading inputs
@@ -11,8 +13,9 @@ rl.setPrompt('>');
 rl.prompt(); 
 rl.on('line', reply =>{
   console.log(`you said ${reply}`);
-  var location= reply.match(/(\bin|of|at\b) (\w+)/);
-  console.log(location[2]);
+  var location= reply.match(/(w|W)hat.*\b(in|of|at)\b (\w+)/, "i");
+  console.log('location:', location[3]);
   console.log();
   rl.prompt(); 
 });
+
